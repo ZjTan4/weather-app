0c7f2fa44d55a0c4ad99bfcd7d8e6308afad7d55
Zijie Tan <ztan4@ualberta.ca>
2025-01-27 14:43:09 -0700

    added google api call

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-backend: Node.js + Express
-on render
-frontend: React + Vite + tailwindcss
-on Vercel
-MongoDB Atlas, Tomorow.io
+Backend: Node.js + Express
+Deployed on Render under https://weather-app-ut4m.onrender.com
+NoSql Database hosted on MongoDB Atlas
+Weather API provided by: Tomorow.io (for realtime weather and forecast), Open-Meteo (for historical data)
+
+Frontend: React + Vite + tailwindcss
+Deployed on Vercel under https://weather-app-two-sage-56.vercel.app/
--- frontend/src/sections/MainSection.jsx
+++ frontend/src/sections/MainSection.jsx
@@ -8,6 +8,7 @@ import ForecastCard from "../components/ForecastCard";
 const MainSection = () => {
     const [weather, setWeather] = useState(null);
     const [forecast, setForecast] = useState(null);
+    const mapApiKey = import.meta.env.VITE_MAP_APIKEY
 
     const handleSearch = async (location) => {
         try {
@@ -35,6 +36,25 @@ const MainSection = () => {
                 {weather && <WeatherCard weather={weather} />}
                 {forecast && <ForecastCard forecast={forecast} />}
             </div>
+            {
+                weather && (
+                    <div className="mt-10 w-full max-w-4xl mx-auto rounded-lg overflow-hidden shadow-lg bg-white">
+                        <div className="p-4 border-b border-gray-200">
+                            <h3 className="text-lg font-semibold text-gray-800">Location Map</h3>
+                        </div>
+                        <div className="relative w-full h-[450px]">
+                            <iframe
+                                className="absolute inset-0 w-full h-full"
+                                style={{ border: 0 }}
+                                loading="lazy"
+                                allowFullScreen
+                                referrerPolicy="no-referrer-when-downgrade"
+                                src={`https://www.google.com/maps/embed/v1/place?key=${mapApiKey}&q=${weather.location.name}`}
+                            />
+                        </div>
+                    </div>
+                )
+            }
         </section>
     );
 };
